972d557af2d1ca773af2f9b912a4be5b601c9245
James Stafford <James@Jamess-Air.hitronhub.home>
2018-06-28 13:36:32 -0700

    add post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+// ES6 object destructuring of mongoose
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+// Configure port
+let port = process.env.PORT || 3000;
+
+// configure middleware
+app.use(bodyParser.json());
+
+// resource creation
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
